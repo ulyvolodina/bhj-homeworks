6b3c1ef32cdff0c9cf450dccfb76446e28b3b16d
Ulya <ulyvolodina@gmail.com>
2022-08-15 10:21:56 +0300

    fixed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,24 +1,26 @@
-const strike = document.getElementById('kill');
-const miss = document.getElementById('lose');
+const strike = document.getElementById('dead');
+const miss = document.getElementById('lost');
 
-function reload() {
-	strike.textContent = 0;
-	miss.textContent = 0;
+function reloadGame() {
+    strike.textContent = 0;
+    miss.textContent = 0;
 }
 
-const getHole = index => document.getElementById('hole${index}')
+const getHole = index => document.getElementById(`hole${index}`)
 
-for (let i=0; i<9; i++) {
-	const hole = getHole(i);
-	hole.onclick = function() {
-		hole.className.includes('hole_has-mole') ? strike.textContent++ : miss.textContent++;
+for (let i = 1; i < 10; i++) {
+    const hole = getHole(i);
+    hole.onclick = function() {
+        hole.className.includes('hole_has-mole') ? strike.textContent++ : miss.textContent++;
 
-		if (strike.textContent == 10) {
-			window.alert('Победа!');
-			reload();
-		} else if (miss.textContent == 5) {
-			window.alert('Поражение!');
-			reload();
-		}
-	}
+        if (strike.textContent == 10) {
+            window.alert('Вы победили');
+            reloadGame();
+        }
+
+        if (miss.textContent == 5) {
+            window.alert('Вы проиграли');
+            reloadGame()
+        }
+    }
 }
