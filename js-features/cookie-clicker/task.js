570c49a43477ee17f4b2d41ac9ccd92e9db18df5
Ulya <ulyvolodina@gmail.com>
2022-08-18 09:40:09 +0300

    fixed clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -5,12 +5,7 @@ function clicker() {
 	click = click + 1;
 	document.getElementById('clicker__counter').innerHTML = click;
 
-	if (click % 2 == 0) {
-		image.width = 300;
-	}
-	else {
-		image.width = 200;
-	}
+	image.width = click % 2 == 0 ? 300 : 200;
 }
 
 document.getElementById('cookie').onclick = function() {
